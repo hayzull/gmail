0bb6385b0aee804b312de3dad7807ec283bfcbc5
Hazel Esmilla <hesmilla8201@gmail.com>
2016-01-21 23:09:25 -0800

    changed to yahoo instead

--- gmail1.py
+++ gmail1.py
@@ -1,21 +1,38 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
-browser.get('https://gmail.com')
+browser.get('https://yahoo.com')
 
-emailElem = browser.find_element_by_id('Email')
-emailElem.send_keys('dannabob5@gmail.com')
-
-loginElem = browser.find_element_by_id('next')
+loginElem = browser.find_element_by_link_text('Mail')
 loginElem.click()
 
-print('Input your password')
-userPassword = input()
+emailElem = browser.find_element_by_id('login-username')
+emailElem.send_keys('dannabob5@yahoo.com')
 
-passwordElem = browser.find_element_by_id('Passwd')
-passwordElem.send_keys(userPassword)
+passElem = browser.find_element_by_id('login-passwd')
+passElem.send_keys('hellogoodbye')
 
-passwordElem.submit()
+signElem = browser.find_element_by_id('login-signin')
+signElem.click()
 
-composeElem = browser.find_element_by_id('z0')
+composeElem = browser.find_element_by_id('Compose')
 composeElem.click()
+
+print('Who are you sending this email to?')
+sendTo = input()
+
+sendtoElem = browser.find_element_by_id('to-field')
+sendtoElem.send_keys(sendTo)
+
+print('What is the subject?')
+subjectLine = input()
+
+subjectElem = browser.find_element_by_id('subject-field')
+subjectElem.send_keys(subjectLine)
+
+print('What is going to be written in the body?')
+bodyLine = input()
+
+sendtoElem = browser.find_element_by_id('rtetext')
+sendtoElem.send_keys (bodyLine)
+
